7361f328d61ba308ea8c3d2522187e1d0f428867
Dhuky <vinicios.mangueira2013@gmail.com>
2021-10-25 10:31:34 -0300

    Update sketch_oct24.ino

--- sketch_oct24/sketch_oct24.ino
+++ sketch_oct24/sketch_oct24.ino
@@ -1,18 +1,23 @@
 #include <Wire.h>
 #include <RTClib.h>
 #include <DHT.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define pinoLDR A0
 #define bomba_reservatorio 7
 #define DHTpin 6
+#define DS18B20 5
 
 #define DHTtype DHT11
 DHT dht (DHTpin, DHTtype);
 RTC_DS3231 rtc;
+OneWire ourWire(DS18B20);
+DallasTemperature sensors(&ourWire);
 
 int tempmax = 0, tempmin = 0;
 int valorLDR = 0,  temperaturaDHT, umidadeDHT;
-float luminosidade = 0;
+float luminosidade = 0, temperaturaDS18B20 = 0;
 char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
 unsigned long previousMillis = 0;
 const long intervalo = 60000;
@@ -20,10 +25,12 @@ const long intervalo = 60000;
 void rotina_irrigacao();
 void leitura_dht();
 void leituraldr();
+void leitura_ds18b20();
 
 void setup() {
   dht.begin();
   rtc.begin();
+  sensors.begin();
   Serial.begin(9600);
 
   pinMode(bomba_reservatorio, OUTPUT);
@@ -32,14 +39,15 @@ void setup() {
 }
 
 void loop () {
-  
+
   unsigned long currentMillis = millis();
   DateTime now = rtc.now();
 
   leitura_dht();
   rotina_irrigacao();
   leituraldr();
-  
+  leitura_ds18b20();
+
   if (currentMillis - previousMillis >= intervalo) {
     previousMillis = currentMillis;
 
@@ -67,6 +75,10 @@ void loop () {
     Serial.print ("Umidade: ");
     Serial.print (umidadeDHT);
     Serial.println(" %");
+
+    Serial.print("Temperatura solução nutritiva: ");
+    Serial.print(temperaturaDS18B20);
+    Serial.println(" C°");
 
     Serial.println("");
   }
