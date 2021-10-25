e5d2d854618af18c871c78a4a96cbf41e86cee75
Dhuky <vinicios.mangueira2013@gmail.com>
2021-10-25 10:31:21 -0300

    Update funcoes_sensores.ino

--- sketch_oct24/funcoes_sensores.ino
+++ sketch_oct24/funcoes_sensores.ino
@@ -1,7 +1,7 @@
 
-void leituraldr(){
-    valorLDR = analogRead(pinoLDR);
-    luminosidade = map(valorLDR, 1024, 0, 0, 100);
+void leituraldr() {
+  valorLDR = analogRead(pinoLDR);
+  luminosidade = map(valorLDR, 1024, 0, 0, 100);
 }
 
 void leitura_dht() {
@@ -9,4 +9,9 @@ void leitura_dht() {
   if (dht.readTemperature() > tempmax) tempmax = dht.readTemperature();
   if (dht.readTemperature() < tempmax) tempmin = dht.readTemperature();
   umidadeDHT = dht.readHumidity();
+}
+
+void leitura_ds18b20() {
+  sensors.requestTemperatures();
+  temperaturaDS18B20 = sensors.getTempCByIndex(0);
 }
